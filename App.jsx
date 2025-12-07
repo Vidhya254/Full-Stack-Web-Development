57341d775d70822ef6e4dac9f67d16e3da119ce0
Vidhya254 <vidhyadharshini04@gmail.com>
2025-12-07 18:42:27 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,33 +1,4 @@
-import React from "react";
-import ExamRegistrationForm from "./FullStack/React/ExamRegistrationForm";
-import bg from "./assets/exam.jpg";
-
-function App() {
-  return (
-    <div style={styles.page}>
-      <div style={styles.overlay}></div>
-      <ExamRegistrationForm />
-    </div>
-  );
+export default function Welcome()
+{
+  return(<h1>Hai Vidhya</h1>);
 }
-
-const styles = {
-  page: {
-    backgroundImage: `url(${bg})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "relative",
-  },
-  overlay: {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    backgroundColor: "rgba(0,0,0,0.45)", // fixed!
-  },
-};
-
-export default App;
